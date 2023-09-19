10162795d7caba7f8f0b6d0c6d0bd4803771a6f4
SongYunTan <songyun.tan@gmail.com>
2023-09-20 00:03:05 +0800

    Add origin

--- backend/urlshortener/__init__.py
+++ backend/urlshortener/__init__.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 def create_app():
     urlshortener_app = Flask(__name__)
     urlshortener_app.register_blueprint(urlshortener.app)
-    CORS(urlshortener_app)
+    CORS(urlshortener_app, origins=["https://baby-url-7d4727a2b0fa.herokuapp.com"])
     return urlshortener_app
 
 app = Blueprint('backend', __name__, url_prefix='/')
